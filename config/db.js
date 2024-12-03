2106b076751522d5cc3f49fbac20701b3f7e4174
ShubhamMore6688 <shubhamdmore03@gmail.com>
2024-12-03 13:19:27 +0530

    changing database configuratio for deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'school_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
