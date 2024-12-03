924f5a628a15059d8033143bd48d7c412f810714
ShubhamMore6688 <shubhamdmore03@gmail.com>
2024-12-03 12:55:10 +0530

    changes in model

---
+++ models/schoolModel.js
@@ -0,0 +1,13 @@
+import pool from '../config/db.js';
+
+export async function addSchool(name, address, latitude, longitude) {
+  const query = 'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)';
+  const [result] = await pool.execute(query, [name, address, latitude, longitude]);
+  return result.insertId;
+}
+
+export async function getAllSchools() {
+  const query = 'SELECT * FROM schools';
+  const [rows] = await pool.query(query);
+  return rows;
+}
